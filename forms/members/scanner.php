3629bd181f71b2402127390baab899ea86b34e46
Blacker <oleg_frolov@mail.ru>
2020-12-17 12:10:34 +0300

    update

--- forms/members/scanner.php
+++ forms/members/scanner.php
@@ -1,6 +1,7 @@
 <html>
 <wb-module  wb="{'module':'barcode'}" />
 <script>
+$(document).on("modBarcode");
 $(document).on("modBarcode",function(e,code){
       wbapp.ajax({url:"/cms/ajax/form/members/view/"+code,"html":".content-body"});
 });
--- forms/visits/scanner.php
+++
@@ -1,9 +0,0 @@
-<html>
-<wb-module wb="{'module':'barcode'}" />
-<script>
-$(document).on("modBarcode",function(e,code){
-      $(e).data('ready',true);
-      wbapp.ajax({url:"/cms/ajax/form/members/view/"+code,"html":".content-body"});
-});
-</script>
-</html>
