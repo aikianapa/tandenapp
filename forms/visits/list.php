0b8fb38fb305b7cae3ae459776bd39a26b6398dc
Blacker <oleg_frolov@mail.ru>
2021-01-14 11:48:54 +0300

    update

--- forms/visits/list.php
+++ forms/visits/list.php
@@ -48,31 +48,10 @@
             </div>
           </div>
         </div>
-
-        {{#if pagination}}
-          {{#if @last===@index}}
-
-            <ul class="pagination mg-b-0 mt-3">
-              {{#each pagination}}
-                {{#if this.label=="prev" }}
-                  <li class="page-item">
-                    <a class="page-link page-link-icon" data-page="{{this.page}}" href="#"><i class="fa fa-chevron-left"></i></a>
-                  </li>
-                  {{elseif this.label == "next"}}
-                  <li class="page-item">
-                    <a class="page-link page-link-icon" data-page="{{this.page}}" href="#"><i class="fa fa-chevron-right"></i></a>
-                  </li>
-                {{else}}
-                  <li class="page-item">
-                    <a class="page-link" data-page="{{this.page}}" href="#">{{this.label}}</a>
-                  </li>
-                {{/if}}
-              {{/each}}
-            </ul>
-
-          {{/if}}
-        {{/if}}
       </wb-foreach>
+      <wb-jq wb="{'append':'#{{_form}}List template','render':'client'}" >
+        <wb-snippet wb-name="pagination"/>
+      </wb-jq>
     </div>
 
 
