6316c87b9e7d295178c305dfa2f8bd0647a21771
Blacker <oleg_frolov@mail.ru>
2021-03-22 12:43:15 +0300

    update

--- functions.php
+++ functions.php
@@ -2,4 +2,9 @@
 function modCmsBeforeShow(&$cms) {
 		$cms->find('title')->after('<link rel="manifest" href="/manifest.json">');
 }
+
+function dateout($date) {
+    return date('d/m/Y', strtotime($date));
+}
+
 ?>
