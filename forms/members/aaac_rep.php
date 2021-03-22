5c68db1ceca4c9ab21a6414c5f08d93817490b86
Blacker <oleg_frolov@mail.ru>
2021-03-22 17:44:37 +0300

    update

--- forms/members/aaac_rep.php
+++ forms/members/aaac_rep.php
@@ -1,11 +1,22 @@
 <html>
 <table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <td>№</td>
+            <td>ФИО</td>
+            <td>Год рождения</td>
+            <td>Ребёнок</td>
+            <td>Сумма взноса</td>
+        </tr>
+    </thead>
     <tbody>
     <wb-foreach wb="table=members&sort=name" wb-filter="{'archive':'','aaac':'on'}">
     <tr>
         <td>{{_ndx}}</td>
-        <td>{{name}}</td>
-        <td>{{aaac_amount}}</td>
+        <td>{{name}} <span class="badge badge-warning" wb-if = "images.0.img == ''">Нет фото</span></td>
+        <td>{{ date("Y",strtotime({{bdate}})) }} г.р.</td>
+        <td><span wb-if="child == 'on'">+</span></td>
+        <td>{{aaac_amount}} ₽</td>
     </tr>
     </wb-foreach>
     </tbody>
